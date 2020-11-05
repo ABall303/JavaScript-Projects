b99d032540c796c2a4eb9d715d3294fc2fa779ed
ABall303 <65334404+ABall303@users.noreply.github.com>
2020-11-04 16:15:23 -0800

    Delete main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++
@@ -1,6 +0,0 @@
-document.write("Click Button to See Function!");                                 //Instructions to see function//
-function My_First_Function() {                                                   //Name of function//
-    var str = "My First Function!";                                             //results of activating function//
-    var result = str.fontcolor ("green");                                       //Font color of funtion results//
-    document.getElementById("Green_Text").innerHTML = result;                   //connecting fuction resutls to HTML//
-}
